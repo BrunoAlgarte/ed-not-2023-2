279859e8a4f0276c26b67da7cf77f4c8f25a7dc5
Bruno Algarte Inacio <br_inacio@magazineluiza.intranet>
2023-08-28 22:04:39 -0300

    AULA 28_08_2023

--- 05_Busca Sequencial.py
+++ 05_Busca Sequencial.py
@@ -1,69 +1,71 @@
 def busca_sequencial(lista, val):
-
     """
-    Função que realiza uma busca sequncial em uma lista procurando um val.
-    Se val for encontrado, retorna a posição de val na lista
-    Caso contrário, retorna o valor da convencional -1.
-
+    Função que realiza uma busca sequencial em uma lista
+    procurando por val.
+    Se val for encontrado, retoma a posição de val na lista.
+    Caso contrário, retorna o valor convencional = 1.
     """
-   # Percorre a lista do início ao fim urando o range() e len()
-   # (precisamos ter acesso ás posições dos elementos)
+
+    # Percorre a lista do início ao fim usando range() e len()
+    # (precisamos ter acesso às posições dos elementos)
     for pos in range(len(lista)):
-       # Encontrou val: retorna a posição onde foi encontrado
-        if val == lista[pos]: 
-           return pos
-   # <--CUIDADO COM A INDENTAÇÂO AQUI
-   # Percorreu toda a lista e não encontrou val: retorna -1
-        return -1
-#######################################################################################################
+        # Encontrou val: retorna a posição onde foi encontrado
+        if val == lista[pos]:
+            return pos
+    # <--- CUIDADO COM A IDENTAÇÃO AQUI
+    # pERCORREU TODA A LISTA E NÃO ENCONTROU VAL: RETORNA -1
+    return -1
+    #######################################
 
-# Para a busca sequencial, a lista NÂO PRECISA estar ordenada
-nums = [9, 21, 33, 12, 0, 18, -3, 30,-15, 6, 3, 27]
+    #Para busca sequencial, a lista NÃO PRECISA estar ordenada
 
-# TESTES
+nums = [9, 21, 33, 12, 0, 18, -3, 30, -15, 6, 3, 27]
 
+#TESTES
 pos30 = busca_sequencial(nums, 30)
 pos_menos3 = busca_sequencial(nums, -3)
 pos19 = busca_sequencial(nums, 19)
 
-print(f"Elemento 30 é encontrado na posição {pos30}.")
-print(f"Elemento -3 é encontrado na posição {pos_menos3}.")
-print(f"Elemento 19 é encontrado na posição {pos19}.")
+print(nums)
+print(f"Elemento 30 encontrado na posição {pos30}.")
+print(f"Elemento -3 encontrado na posição {pos_menos3}.")
+print(f"Elemento 19 encontrado na posição {pos19}.")
 
-print("-" * 40)
+print('-' *20)
 
-# FAZENDO A BUSCA EM UM ARQUIVO COM 1M+ NOMES
+#Fazendo a busca em um arquivo do 1M+ nomes
 
-# Importando a lista de nomes
+#importando a lista de nomes
 import sys
-sys.dont_write_bytecode = True #Impede a criação do cache
+sys.dont_write_bytecode = True #impede criação da cache
+
 from data.nomes import nomes
 from time import time
 
 # Buscando o nome EDSON PEREIRA
 hora_ini = time()
-pos1 = busca_sequencial(nomes, "EDSON PEREIRA")
-print(f"EDSON PERREIRA encontrada na posição {pos1}")
+pos1 = busca_sequencial(nomes, 'EDSON PEREIRA')
 hora_fim = time()
-print(f"Tempo Gasto: {(hora_fim - hora_ini) * 1000}ms\n")
+print(f"EDSON PEREIRA encontrado na posição {pos1}")
+print(f'Tempo gasto: {(hora_fim - hora_ini) * 1000}ms\n')
 
 # Buscando o nome MARIA FERREIRA
 hora_ini = time()
-pos2 = busca_sequencial(nomes, "MARIA FERREIRA")
+pos2 = busca_sequencial(nomes, 'MARIA FERREIRA')
 hora_fim = time()
 print(f"MARIA FERREIRA encontrada na posição {pos2}")
-print(f"Tempo Gasto: {(hora_fim - hora_ini) * 1000}ms\n")
+print(f'Tempo gasto: {(hora_fim - hora_ini) * 1000}ms\n')
 
 # Buscando o nome VALDIR SILVA
 hora_ini = time()
-pos3 = busca_sequencial(nomes, "VALDIR SILVA")
+pos3 = busca_sequencial(nomes, 'VALDIR SILVA')
 hora_fim = time()
-print(f"VALDIR SILVA encontrada na posição {pos3}")
-print(f"Tempo Gasto: {(hora_fim - hora_ini) * 1000}ms\n")
+print(f"VALDIR SILVA encontrado na posição {pos3}")
+print(f'Tempo gasto: {(hora_fim - hora_ini) * 1000}ms\n')
 
-# Buscando o nome da GILCICLEIDE GARCIA
+# Buscando o nome GILCICLEIDE GARCIA
 hora_ini = time()
-pos4 = busca_sequencial(nomes, "GILCICLEIDE GARCIA")
+pos4 = busca_sequencial(nomes, 'GILCICLEIDE GARCIA')
 hora_fim = time()
 print(f"GILCICLEIDE GARCIA encontrada na posição {pos4}")
-print(f"Tempo Gasto: {(hora_fim - hora_ini) * 1000}ms\n")
+print(f'Tempo gasto: {(hora_fim - hora_ini) * 1000}ms\n')
--- 06_busca_binaria.py
+++ 06_busca_binaria.py
@@ -1,3 +1,5 @@
+comps = 0
+
 def busca_binaria(lista, val):
     """
     ALGORITMO DE BUSCA BINÁRIA
@@ -9,6 +11,9 @@ def busca_binaria(lista, val):
     apenas a sublista vazia, quando então se conclui
     que o valor de busca não existe na lista.
     """
+    global comps  #Use a cariável comps do escopo global
+    comps= 0
+
     inicio = 0  # Início da lista
     fim = len(lista) - 1  # Fim da lista
 
@@ -21,17 +26,73 @@ def busca_binaria(lista, val):
         # retornamos a posição do meio, pois o valor de
         # busca foi encontrado 
         if val == lista[meio]:
+            comps += 1
             return meio
         
         # Senão, se o valor de busca é MENOR do que o valor
         # do meio, reinicia a busca na metade da esquerda da lista
         elif val < lista[meio]:
+            comps += 2
             fim = meio - 1
         
         # Por fim, se o valor da busca é MAIOR que o valor
         # do meio, reinicia a busca na metade direita da (sub)lista
         else:
+            comps += 2
             inicio = meio + 1
 
     # Se chegamos até este ponto, o valor de busca NÃO EXISTE na lista
     return -1
+
+#############################################################################
+
+# Para uma busca bínaria, a lista PRECISA estar odenada
+nums = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 47]
+
+pos11 = busca_binaria(nums, 11)
+print(f"Valor 11 é encontrado na posição {pos11}")
+
+pos41 = busca_binaria(nums, 41)
+print(f"Valor 41 é encontrado na posição {pos41}")
+
+pos8 = busca_binaria(nums, 8)
+print(f"Valor 8 é encontrado na posição {pos8}")
+
+print("-"*40)
+
+#Fazendo a busca em um arquivo do 1M+ nomes
+
+#importando a lista de nomes
+import sys
+sys.dont_write_bytecode = True #impede criação da cache
+
+from data.nomes import nomes
+from time import time
+
+# Buscando o nome EDSON PEREIRA
+hora_ini = time()
+pos1 = busca_binaria(nomes, 'EDSON PEREIRA')
+hora_fim = time()
+print(f"EDSON PEREIRA encontrado na posição {pos1}, comparação: {comps}")
+print(f'Tempo gasto: {(hora_fim - hora_ini) * 1000}ms\n')
+
+# Buscando o nome MARIA FERREIRA
+hora_ini = time()
+pos2 = busca_binaria(nomes, 'MARIA FERREIRA')
+hora_fim = time()
+print(f"MARIA FERREIRA encontrada na posição {pos2}, comparação: {comps}")
+print(f'Tempo gasto: {(hora_fim - hora_ini) * 1000}ms\n')
+
+# Buscando o nome VALDIR SILVA
+hora_ini = time()
+pos3 = busca_binaria(nomes, 'VALDIR SILVA')
+hora_fim = time()
+print(f"VALDIR SILVA encontrado na posição {pos3}, comparação: {comps}")
+print(f'Tempo gasto: {(hora_fim - hora_ini) * 1000}ms\n')
+
+# Buscando o nome GILCICLEIDE GARCIA
+hora_ini = time()
+pos4 = busca_binaria(nomes, 'GILCICLEIDE GARCIA')
+hora_fim = time()
+print(f"GILCICLEIDE GARCIA encontrada na posição {pos4}, comparação: {comps}")
+print(f'Tempo gasto: {(hora_fim - hora_ini) * 1000}ms\n')
